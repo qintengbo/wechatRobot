3ba2eeaee8a2b57bf1db1ab4377514eddd8ae3c7
qintengbo <qintb@hua-cloud.com.cn>
2019-07-13 22:29:52 +0800

    修复bug

--- services/initDay.js
+++ services/initDay.js
@@ -6,7 +6,7 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 // 初始化微信每日说
 initDay = async (robot) => {
-  const room = await robot.Room.find({ topic: '测试' });
+  const room = await robot.Room.find({ topic: constant.roomName });
   // 获取每日一句
   let one = await getOne();
   // 获取天气
