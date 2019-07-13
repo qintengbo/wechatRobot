aa8ef34a99b9c48383385cbfc021fcaa444e29f0
qintengbo <qintb@hua-cloud.com.cn>
2019-07-13 22:24:51 +0800

    优化代码

--- services/initDay.js
+++ services/initDay.js
@@ -11,15 +11,21 @@ initDay = async (robot) => {
   let one = await getOne();
   // 获取天气
   let weather = await getWeather();
+  let str1 = `【今日天气】<br>${weather.weatherTips}<br>${weather.todayWeather}<br>【每日一句】<br>${one}`;
+  await room.say(str1);
+
   // 获取知乎热榜
   let hot = await getZhiHuHot();
+  if (!hot) {
+		await delay(2000);
+  	await room.say('@随遇而安 知乎热榜获取失败，请及时解决');
+  	return;
+  }
   let hotContent = '';
   for (let i = 0; i < hot.length; i++) {
   	hotContent += `${i + 1}. ${hot[i].title}<br>(${hot[i].hotNum})<br>详情链接：${hot[i].url}<br>`;
   }
-  let str1 = `【今日天气】<br>${weather.weatherTips}<br>${weather.todayWeather}<br>【每日一句】<br>${one}`;
   let str2 = `【知乎热榜前十】<br>${hotContent}`;
-  await room.say(str1);
   await delay(2000);
   await room.say(str2);
 }
